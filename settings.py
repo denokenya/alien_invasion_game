29b6215c7c6f735a1d03ee16006ca6cb7a5144ed
denokenya <dennismuteti25@gmail.com>
2022-02-22 15:55:13 +0300

    code refactor

--- settings.py
+++ settings.py
@@ -4,18 +4,19 @@ class Settings:
     def __init__(self):
         """Initialize the game's settings."""
         #Screen settingss
-        self.screen_width =1200
-        self.screen_height =800
-        self.bg_color =(230,230,230)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
 
         #Ship settings
-        self.ship_speed =1.5
+        self.ship_speed = 1.5
 
         #Bullet settings
-        self.bullet_speed =1.0
-        self.bullet_width =3
-        self.bullet_height =15
-        self.bullet_color =(60,60,60)
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
         
 
     
