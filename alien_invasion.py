0e1cc883d4bb892d8d383ae2ff25a48d365db1ba
denokenya <dennismuteti25@gmail.com>
2022-02-22 13:44:59 +0300

    code refactor

--- alien_invasion.py
+++ alien_invasion.py
@@ -40,25 +40,30 @@ class AlienInvasion:
             pygame.display.flip()
 
     def _check_events(self):
+        """Respond to keypresses and mouse events"""
         for event in pygame.event.get():
                 if event.type ==  pygame.QUIT:
                     sys.exit()
-
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:  
+                    self._check_keyup_events(event)
 
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False 
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False     
-
+    def _check_down_events(self,event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key  == pygame.K_LEFT:
+            self.ship.moving_left = True    
+    
+    def _check_keyup_events(self,event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False    
+        
                          
-                          
-
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
